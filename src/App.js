902d976e4565951368c40af0fc3ddb06f281c7b9
Alan Dong <ldong@gatech.edu>
2018-10-11 03:20:08 -0700

    Add redux to create-react-app
    - Follow https://medium.com/backticks-tildes/setting-up-a-redux-project-with-create-react-app-e363ab2329b8 with some changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
+/*
+ src/App.js
+*/
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import { simpleAction } from './actions/simpleAction';
+
+const mapStateToProps = state => ({
+ ...state
+})
+
+const mapDispatchToProps = dispatch => ({
+ simpleAction: () => dispatch(simpleAction())
+})
+
+
 
 class App extends Component {
+  simpleAction = (event) => {
+   this.props.simpleAction();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     <div className="App">
+
+      <pre>
+       {
+        JSON.stringify(this.props)
+       }
+      </pre>
+
+      <button onClick={this.simpleAction}>Test redux action</button>
+
+     </div>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
